e470d71d75796deca085b19be4cb0269378c2a45
Lakshayaa <lakshhayaaa@gmail.com>
2026-04-16 18:13:05 +0530

    Middleware cors added for fe integration

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from routes.auth import router as auth_router
 from routes.query import router as query_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI(title="Case-Based AI API", description="API for managing entrepreneurial cases, user queries, and AI responses.")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 app.include_router(auth_router)
 app.include_router(query_router)
 
